6c12f3c1680b432f6afe2375318540d6cc5fa8f0
Radek Podgorny <radek@podgorny.cz>
2011-12-20 16:17:16 +0100

    wrong zone file was being checked

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+import py2exe
+
+from nsupdate import __version__ as version
+
+setup(
+	console = ['nsupdate.py', ],
+	version = version,
+	zipfile = None,
+	options = {'py2exe': {'bundle_files': 1}}
+)
