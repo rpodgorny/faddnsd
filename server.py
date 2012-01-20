f56ee28e67e6df88f13918accd9998554f80ccf9
Radek Podgorny <radek@podgorny.cz>
2012-01-20 16:48:28 +0100

    added dedicated python server for the new version

---
+++ server.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+__version__ = '0.0'
+
+import cherrypy
+import os
+import os.path
+import datetime
+
+path_prefix = '/tmp'
+
+class NsUpdateServer(object):
+	@cherrypy.expose
+	def index(self, version=None, host=None, domain=None, *args, **kwargs):
+		dt = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+		remote_addr = cherrypy.request.remote.ip
+
+		dn = '%s/%s' % (path_prefix, domain)
+		if not os.path.isdir(dn): os.mkdir(dn)
+
+		fn = '%s/%s' % (dn, host)
+		f = open(fn, 'w')
+
+		f.write('%s %s %s\n' % (version, dt, remote_addr))
+		f.write('%s %s\n' % (host, domain))
+
+		for af in 'ether', 'inet', 'inet6':
+			if not af in kwargs: continue
+
+			for a in kwargs[af]:
+				f.write('%s %s\n' % (af, a))
+			#endfor
+		#endfor
+
+		f.close()
+	#enddef
+#endclass
+
+cherrypy.server.socket_host = '0.0.0.0'
+cherrypy.quickstart(NsUpdateServer())
