e446595270b114dcccdf1573894525fe0daf38aa
Radek Podgorny <radek@podgorny.cz>
2012-07-21 11:44:12 +0200

    bugfix for eating records

---
+++ nsupdate-zone.py
@@ -0,0 +1,177 @@
+#!/usr/bin/python
+
+__version__ = '0.1'
+
+import sys
+import glob
+import re
+import subprocess
+import os
+
+def check_zone(zone, fn):
+	cmd = 'named-checkzone %s %s' % (zone, fn) 
+
+	try:
+		out = subprocess.check_output(cmd, shell=True)
+	except subprocess.CalledProcessError:
+		print cmd
+		return False
+	#endtry
+
+	print out
+	return True
+#enddef
+
+class Change:
+	def __init__(self):
+		self.ttl = '1h'
+		self.processed = False
+	#enddef
+
+	def read_from_file(self, fn):
+		self.fn = fn
+
+		f = open(fn, 'r')
+		self.version, self.datetime, self.remote_addr = f.readline().split()
+		self.host, self.domain = f.readline().split()
+
+		self.addrs = []
+		for line in f:
+			af, a = line.split()
+			self.addrs.append((af, a))
+		#endfor
+
+		f.close()
+	#enddef
+#endclass
+
+def main():
+	serial_done = False
+
+	zone = sys.argv[1]
+	zone_fn = sys.argv[2]
+	changes_dir = sys.argv[3]
+
+	out_fn = '/tmp/tmp.zone' # TODO: rename variable and make the value random
+
+	changes = []
+	for i in glob.glob(changes_dir+'/*'):
+		c = Change()
+		c.read_from_file(i)
+		changes.append(c)
+	#endfor
+
+	if not changes:
+		print 'no change files found, doing nothing'
+		return
+	#endif
+
+	for i in changes: print i.host, i.domain, i.addrs
+	
+	cmd = 'cp -a %s %s' % (zone_fn, out_fn)
+	subprocess.call(cmd, shell=True)
+
+	zone_file = open(zone_fn, 'r')
+	out_file = open(out_fn, 'w')
+
+	for line in zone_file:
+		if 'erial' in line:
+			if not serial_done:
+				#serial = re.match('.*[0-9]+.*', line)
+				serial = re.search('(\d+)', line).group(0)
+				serial = int(serial)
+				line = line.replace(str(serial), str(serial+1))
+
+				out_file.write(line)
+
+				serial_done = True
+
+				print 'serial: %s -> %s' % (serial, serial+1)
+			#endif
+
+			continue
+		#endif
+
+		change = None
+		for i in changes:
+			if not line.startswith(i.host+'\t'): continue
+			if i.domain != zone: continue
+			change = i
+			break
+		#endfor
+
+		# no match
+		if change is None:
+			out_file.write(line)
+			continue
+		#endif
+
+		m = re.match('(\S+)\t(\S+)\t(\S+)\t(\S+)', line)
+		if not m:
+			print 'record for \'%s\' in wrong format, skipping' % line
+			out_file.write(line)
+			continue
+		#endif
+
+		if change.processed: continue
+
+		print 'updating %s' % change.host
+
+		#m_host, m_ttl, m_typ, m_addr = m.groups()
+		#print m
+		#print m.groups()
+
+		out = ''
+		for af,a in change.addrs:
+			if af == 'inet':
+				af = 'a'
+			elif af == 'inet6':
+				af = 'aaaa'
+			else:
+				print 'unsupported record type %s' % af
+				continue
+			#endif
+
+			host = change.host.lower()
+			ttl = change.ttl.upper()
+			af = af.upper()
+
+			out += '%s\t%s\t%s\t%s ; %s\n' % (host, ttl, af, a, change.datetime)
+			print '%s %s' % (af, a)
+		#endfor
+
+		if out:
+			out_file.write(out)
+			change.processed = True
+		else:
+			print 'change contains no usable data, keeping old record'
+			out_file.write(line)
+		#endif
+
+	#endfor
+
+	zone_file.close()
+	out_file.close()
+
+	if not check_zone(zone, out_fn):
+		print 'zone check error!'
+		return
+	#endif
+
+	cmd = 'mv %s %s' % (out_fn, zone_fn)
+	subprocess.call(cmd, shell=True)
+
+	cmd = 'rndc reload %s' % zone
+	subprocess.call(cmd, shell=True)
+
+	for c in changes:
+		if c.processed:
+			os.remove(c.fn)
+			continue
+		#endif
+
+		print '%s not processed!' % c.host
+	#endfor
+#enddef
+
+if __name__ == '__main__': main()
---
+++ nsupdate.py
@@ -0,0 +1,281 @@
+#!/usr/bin/python
+
+__version__ = '0.5'
+
+import sys
+import socket
+import urllib
+import time
+import getopt
+import re
+import tray
+import logging
+from iniparser import IniParser
+
+# TODO: not working
+#def log_e(t, v, tb): logging.exception('unhandled exception!')
+#sys.excepthook = log_e
+
+class Config:
+	def __init__(self):
+		self.domain = None
+		self.host = socket.gethostname().lower()
+		self.interval = 600
+		self.url_prefix = []
+	#enddef
+	
+	def read_from_ini(self, fn):
+		ini = IniParser()
+		ini.read(fn)
+
+		self.domain = ini.get('General', 'Domain', self.domain)
+		self.host = ini.get('General', 'Host', self.host)
+		self.interval = ini.getint('General', 'Interval', self.interval)
+		self.url_prefix = ini.get('General', 'UrlPrefix', self.url_prefix)
+	#enddef
+
+	def getopt(self, argv):
+		opts, args = getopt.getopt(argv, 'd:h:i:u:', ('domain=', 'host=', 'interval=', 'url-prefix='))
+		for o,a in opts:
+			if o in ('-d', '--domain'):
+				self.domain = a.lower()
+			elif o in ('-h', '--host'):
+				self.host = a.lower()
+			elif o in ('-i', '--interval'):
+				self.interval = int(a)
+			elif o in ('-u', '--url-prefix'):
+				self.url_prefix.append(a)
+			#endif
+		#endfor
+
+		if not self.url_prefix: self.url_prefix = ['http://podgorny.cz:8765/', ]
+	#enddef
+
+	def check(self):
+		if not self.domain: return 'domain not specified!'
+	#enddef
+	
+	# TODO: move this to some common module
+	def __str__(self):
+		l = []
+
+		for k,v in vars(self).items():
+			l.append('%s=\'%s\'' % (k, v))
+		#endfor
+
+		return ', '.join(l)
+	#enddef 
+#endclass
+
+cfg = Config()
+
+# TODO: this is disabled because it does not work when compiled as windows application
+def call_old(cmd):
+	logging.debug('calling: %s', cmd)
+	
+	import subprocess
+
+	try:
+		return subprocess.check_output(cmd, shell=True)
+	except AttributeError:
+		# python < 2.7
+		p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+		p.wait()
+		return p.communicate()[0]
+	#endtry
+#enddef
+
+def call(cmd):
+	logging.debug('calling: %s', cmd)
+
+	import os
+	f = os.popen(cmd)
+	return f.read()
+#enddef
+
+def get_addrs_windows():
+	ret = []
+
+	lines = call('netsh interface ipv6 show address')
+
+	for word in lines.split():
+		word = word.strip().lower()
+
+		if not ':' in word: continue
+		if not word.startswith('200'): continue
+
+		ret.append({'af': 'inet6', 'a': word})
+	#endfor
+	
+	lines = call('ipconfig /all')
+	for word in lines.split():
+		word = word.strip().lower()
+		if not re.match('..-..-..-..-..-..', word): continue
+
+		word = word.replace('-', ':')
+		ret.append({'af': 'ether', 'a': word})
+	#endfor
+	
+	return ret
+#enddef
+
+def get_addrs_linux():
+	ret = []
+
+	lines = call('ip addr').split('\n')
+
+	for line in lines:
+		line = line.strip()
+
+		if not 'ether' in line \
+		and not 'inet' in line:
+			continue
+		#endif
+
+		addr_type, addr, _ = line.split(' ', 2)
+		addr_type = addr_type.lower()
+		addr = addr.lower()
+
+		if 'ether' in addr_type:
+			addr_type = 'ether'
+		elif 'inet6' in addr_type:
+			addr_type = 'inet6'
+		elif 'inet' in addr_type:
+			addr_type = 'inet'
+		else:
+			logging.warning('unknown address type!')
+		#endif
+
+		try:
+			addr = addr.split('/')[0]
+		except: pass
+
+		if addr_type == 'ether':
+			if addr == '00:00:00:00:00:00': continue
+		elif addr_type == 'inet':
+			if addr.startswith('127.'): continue
+			if addr.startswith('10.'): continue
+			if addr.startswith('192.168.'): continue
+		elif addr_type == 'inet6':
+			if addr.startswith('::1'): continue
+			if addr.startswith('fe80:'): continue
+		#endif
+
+		ret.append({'af': addr_type, 'a': addr})
+	#endfor
+
+	return ret
+#enddef
+
+def init_logging():
+	logger = logging.getLogger()
+	logger.setLevel(logging.DEBUG)
+
+	sh = logging.StreamHandler()
+	fh = logging.FileHandler('nsupdate.log')
+
+	sh.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+	fh.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+
+	logger.addHandler(sh)
+	logger.addHandler(fh)
+#enddef
+
+def main():
+	init_logging()
+
+	logging.info('*' * 40)
+	logging.info('starting nsupdate v%s',  __version__)
+	
+	cfg.read_from_ini('nsupdate.ini')
+
+	cfg.getopt(sys.argv[1:])
+	err = cfg.check()
+	if err:
+		logging.error(err)
+		return
+	#endif
+	
+	logging.info('%s', cfg)
+
+	if sys.platform == 'win32':
+		logging.info('detected win32')
+		get_addrs = get_addrs_windows
+
+		import tray
+		tray.run('nsupdate.png', 'nsupdate v%s' % __version__)
+	elif sys.platform == 'linux2':
+		logging.info('detected linux2')
+		get_addrs = get_addrs_linux
+	else:
+		logging.error('unknown platform!')
+		return
+	#endif
+
+	addr_life = {}
+
+	try:
+		while not tray._exit:
+			t = time.time()
+
+			addrs = get_addrs()
+			logging.debug(addrs)
+
+			for url in cfg.url_prefix:
+				# TODO: for the next version?
+				#recs = []
+				#for i in addrs:
+				#	r = []
+				#	for k,v in i.items(): r.append('%s=%s' % (k, v))
+				#	r = ','.join(r)
+				#	recs.append(r)
+				#endfor
+				#logging.debug('recs = %s', recs)
+
+				a = {'ether': [], 'inet': [], 'inet6': []}
+				for i in addrs:
+					if not i['af'] in a: continue
+					a[i['af']].append(i['a'])
+				#endfor
+
+				d = {
+					'version': __version__,
+					'host': cfg.host,
+					'domain': cfg.domain,
+					#'records': recs
+				}
+				d.update(a)
+				url += '?' + urllib.urlencode(d, True)
+
+				logging.debug(url)
+
+				try:
+					u = urllib.urlopen(url)
+
+					if 'OK' in ''.join(u):
+						logging.info('OK')
+					else:
+						logging.warning('NOT OK')
+						for i in u: logging.debug(i.strip())
+					#endif
+				except:
+					logging.exception('urllib exception!')
+				#endtry
+			#endfor
+
+			logging.info('sleeping for %ss', cfg.interval)
+			while time.time() - t < cfg.interval:
+				if tray._exit: break
+				time.sleep(1)
+			#endwhile
+		#endwhile
+	except KeyboardInterrupt:
+		logging.info('keyboard interrupt!')
+	#endtry
+
+	if sys.platform == 'win32':
+		tray.exit()
+	#endif
+#enddef
+
+if __name__ == '__main__': main()
