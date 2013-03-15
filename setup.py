d14c1a001d58227570701eb04764edca9b57169e
Radek Podgorny <radek@podgorny.cz>
2013-03-15 17:01:07 +0100

    experimental distutils setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from distutils.core import setup
-import py2exe
+#import py2exe
 
 from nsupdate import __version__
 
+'''
 setup(
 	console = ['nsupdate.py', ],
 	version = __version__,
@@ -15,4 +16,15 @@ setup(
 	version = __version__,
 	zipfile = None,
 	options = {'py2exe': {'bundle_files': 1}}
+)
+'''
+
+setup(
+	name = 'dnsupdater',
+	version = __version__,
+	#modules = ['nsupdate.py'],
+	scripts = ['nsupdate.py'],
+	data_files = [
+		('/etc', ['nsupdate.ini',]),
+	]
 )
