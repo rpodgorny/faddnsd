4afc80da2c1f95906c6f2d92e6b53f138362cfea
Radek Podgorny <radek@podgorny.cz>
2013-12-20 01:48:59 +0100

    more progress on the tray client

--- nsupdate_tray.py
+++ nsupdate_tray.py
@@ -8,6 +8,31 @@ from PySide.QtGui import *
 import logging
 
 
+class MyTray(QSystemTrayIcon):
+	def __init__(self, app):
+		icon = QIcon('nsupdate.png')
+		super().__init__(icon)
+
+		self.app = app
+
+		menu = QMenu()
+		menu.addAction('Force refresh', self.on_refresh)
+		menu.addAction('Exit', self.on_exit)
+		self.setContextMenu(menu)
+
+		self.show()
+	#enddef
+
+	def on_refresh(self):
+		pass
+	#enddef
+
+	def on_exit(self):
+		self.app.quit()
+	#enddef
+#endclass
+
+
 def logging_setup():
 	logging.basicConfig(level='DEBUG')
 #enddef
@@ -21,8 +46,7 @@ def main():
 
 	app = QApplication(sys.argv[1:])
 
-	icon = QIcon('nsupdate.png')
-	tray = QSystemTrayIcon(icon)
+	tray = MyTray(app)
 
 	app.exec_()
 
