4fd49b7b6f50b13a520548b705ac6f250ebafc9a
Radek Podgorny <radek@podgorny.cz>
2014-11-11 22:02:29 +0100

    first batch of cleanups

--- setup.py
+++ setup.py
@@ -1,56 +1,18 @@
-import sys
+#!/usr/bin/python3
+
+from setuptools import setup, find_packages
+
 from version import __version__
 
-
-if sys.platform == 'win32':
-	from cx_Freeze import setup, Executable
-
-	base = 'Win32GUI'
-
-	executables = [
-		Executable(
-			script='faddnsc',
-			appendScriptToExe=True,
-			appendScriptToLibrary=False,
-			compress=True,
-		),
-		Executable(
-			script='faddnsc_gui',
-			appendScriptToExe=True,
-			appendScriptToLibrary=False,
-			compress=True,
-			base=base
-		)
-	]
-
-	setup(
-		name = 'faddns',
-		version = __version__,
-		options = {
-			'build_exe': {
-				'includes': ['re', ],
-				'create_shared_zip': False,
-				'compressed': True,
-				'include_msvcr': True,
-				'include_files': ['faddns.png', 'faddnsc.ini']
-			},
+setup(
+	name = 'faddns',
+	version = __version__,
+	options = {
+		'build_exe': {
+			'compressed': True,
+			'include_files': ['faddns.png', 'etc/faddnsc.conf']
 		},
-		executables = executables,
-	)
-else:
-	from setuptools import setup, find_packages
-
-	setup(
-		name = 'faddns',
-		version = __version__,
-		options = {
-			'build_exe': {
-				'compressed': True,
-				'include_files': ['faddns.png', 'faddnsc.ini']
-			},
-		},
-		scripts = ['faddnsc'],
-		#packages = find_packages(),
-		py_modules = ['cfg', 'faddns', 'version'],
-	)
-#endif
+	},
+	scripts = ['faddnsc', 'faddnsd', 'faddns_update_zone'],
+	packages = find_packages(),
+)
