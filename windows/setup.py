f179d8a7525ec1711420fc4b12e639830293120d
Radek Podgorny <radek@podgorny.cz>
2014-11-13 12:31:54 +0100

    add windows-specific setup.py

---
+++ windows/setup.py
@@ -0,0 +1,38 @@
+import sys
+from cx_Freeze import setup, Executable
+
+from version import __version__
+
+
+base = 'Win32GUI'
+
+executables = [
+	Executable(
+		script='faddnsc',
+		appendScriptToExe=True,
+		appendScriptToLibrary=False,
+		compress=True,
+	),
+	Executable(
+		script='faddnsc_gui',
+		appendScriptToExe=True,
+		appendScriptToLibrary=False,
+		compress=True,
+		base=base
+	)
+]
+
+setup(
+	name = 'faddns',
+	version = __version__,
+	options = {
+		'build_exe': {
+			'includes': ['re', ],
+			'create_shared_zip': False,
+			'compressed': True,
+			'include_msvcr': True,
+			'include_files': ['faddns.png', 'faddnsc.ini']
+		},
+	},
+	executables = executables,
+)
